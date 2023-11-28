29615cc3dcd3282bfe19e6037963cfd373282a0b
brettstaples <bstap1125@gmail.com>
2023-11-28 16:25:58 -0500

    deleted branch

--- calculator.c
+++ calculator.c
@@ -11,6 +11,7 @@ int startAndKeepUsingCalculator(int isStart);
 int stayInButton();
 void squareRoots();
 
+
 int main(int argc, char* argv[]) {
     int isStart = 1;
     int useCalculator = startAndKeepUsingCalculator(isStart);
